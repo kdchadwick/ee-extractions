b647eb18e56a5b9644bf19419f795d43e571425b
Erica McCormick <erica.elmstead@gmail.com>
2021-06-30 15:45:21 -0500

    working on flexible start-end dates per asset: not working yet

--- ee_extractions.py
+++ ee_extractions.py
@@ -19,8 +19,8 @@ ee.Initialize()
 gage = 11134800
 start = '1988-09-30'
 stop = '2019-11-01'
-beginning = 2003
-end = 2004
+#beginning = 2003
+#end = 2004
 
 
 
@@ -35,6 +35,8 @@ def main():
  #   names = locs['Site Name']
     assets = pd.read_csv('data/layers_short.csv')
     assets['scale'] = assets['scale'].astype('float')
+    min_year = assets['beginning_year'].min()
+    max_year = assets['end_year'].max()
 
  #   pts = []
  #   for i in range(len(coordinates)):
@@ -63,14 +65,21 @@ def main():
 
 
     all_points = pd.DataFrame()
-    outputfile = 'exports/watershed.csv'
+    outputfile = 'exports/watershed2.csv'
   # for i in range(len(pts)):
   #   print('Getting data for point={}'.format(i))
-    temp_point = pd.DataFrame({'id':pd.date_range(str(beginning)+"-01-01", str(end)+"-01-01")})#, 'point': names[i]})
+    temp_point = pd.DataFrame({'id':pd.date_range(str(min_year)+"-01-01", str(max_year)+"-01-01")})#, 'point': names[i]})
+    #temp_point = pd.DataFrame({'id':pd.datetime('2010-01-01')})
+   # temp_point = pd.DataFrame(columns = ["id"])
+   # temp_point = temp_point.set_index('id')
+   ## print(temp_point.head())
     for d in range(len(assets['gee_path'])):
       path = assets['gee_path'][d]
       collection_short_name=assets['name'][d]+"_"
+      beginning = assets['beginning_year'][d]
+      end = assets['end_year'][d]
 
+      #print(beginning, end)
       def extract(image):
       # pt = pts[i]
         s = assets['scale'][d]
@@ -83,6 +92,7 @@ def main():
 
       print('Extracting data from asset {}'.format(path))
       annual_temp=pd.DataFrame()
+
       for year in range(beginning, end):
         print('year {}'.format(year))
         yearstart = str(year)
@@ -93,20 +103,30 @@ def main():
         #test = get_data_df(testextract, dateformat=assets['date_format'][d], collection_short_name=collection_short_name)
         d = testextract
         df = pd.json_normalize(d['features'])
-        ##print(df.head())
+        print(df.head())
+"""
         #print(df.head())
         #df = df[df.columns[df.columns.str.contains('index')].append(df.columns[df.columns.str.contains('properties')])]
         #print(df.head())
-        df['id'] = pd.to_datetime(df['properties.system:index'], format= dateformat)
+       # df['id'] = df[df.columns[df.columns.str.contains('system:index')]]#.values()
+        print(df.filter(regex='properties.system:index').columns)
+        df['id'] = df[df.filter(regex='system:index').columns]
+        print(df.head())
+        df['id'] = pd.to_datetime(df['id'], format= dateformat)
         #df = df[df.columns[df.columns.str.contains('id')].append(df.columns[df.columns.str.contains('properties')])]
-        df = df.drop(df.filter(regex='index').columns, axis=1)
-        df = df.set_index('id')
+        #df = df.reset_index()
+        #df = df.set_index('id')
+        #df = df.drop(df.filter(regex='index').columns, axis=1)
         oldnames = df.columns[df.columns.str.contains('properties')]
         df = df[df.columns[df.columns.str.contains('properties')]]
         df.columns = list(map(lambda x: x.replace('properties.', collection_short_name),oldnames))
-        print(df.head())
+        print(df.tail())
         annual_temp = annual_temp.append(df)
-      temp_point = pd.merge(temp_point, annual_temp, how='left', on=['id'])
+        print(annual_temp.head())
+        #      temp_point = pd.merge(temp_point, annual_temp, how='outer', left_index = True, right_index = True)#on=['id'])
+
+      temp_point = pd.merge(temp_point, annual_temp, how='outer', on=['id'])
+      print(temp_point.tail())
       #temp_point.to_csv('exports/{}.csv'.format(collection_short_name), mode='a', header=True)
 
       #print(temp_point.head())
@@ -115,8 +135,6 @@ def main():
 
     all_points.to_csv(outputfile, mode='a', header=True)
 
-
-"""
   def get_data_df(d, dateformat, collection_short_name):
     df = pd.json_normalize(d['features'])
     df = df[df.columns[df.columns.str.contains('id')].append(df.columns[df.columns.str.contains('properties')])]
