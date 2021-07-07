ccc59248fe95d6e1b0bf02db53a60c6887563894
K. Dana Chadwick <kdchadwick@gmail.com>
2021-07-07 11:17:37 -0500

    reorganization for ericas pull request

--- code/api_extractions/interp_et.py
+++ code/api_extractions/interp_et.py
@@ -6,16 +6,16 @@ def interp_et(df):
     if 'pml_Ec' in df.columns:
 
         # calculate ET and ET+interception and x8
-        df['pml_ET'] = (df['pml_Ec'] + df['pml_Es']) * 8
-        df['pml_ET_Ei'] = (df['pml_Ec'] + df['pml_Es'] + df['pml_Ei']) * 8
+        df['pml_ET'] = (df['pml_Ec'] + df['pml_Es'])
+        df['pml_ET_Ei'] = (df['pml_Ec'] + df['pml_Es'] + df['pml_Ei'])
 
         # interp them
         df['pml_ET'] = df['pml_ET'].resample('D').mean().interpolate()
         df['pml_ET_Ei'] = df['pml_ET_Ei'].resample('D').mean().interpolate()
 
     if 'modis_ET' in df.columns:
-        df['modis_ET'] = df['modis_ET'].resample('D').mean().interpolate()
-        df['modis_PET'] = df['modis_PET'].resample('D').mean().interpolate()
+        df['modis_ET'] = df['modis_ET'].resample('D').mean().interpolate() / 8
+        df['modis_PET'] = df['modis_PET'].resample('D').mean().interpolate() /8
 
 
     return df
