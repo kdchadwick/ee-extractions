8d25f6e0dad04ca68e9b70fcc5ecfb4592f4882f
Erica McCormick <erica.elmstead@gmail.com>
2021-07-06 22:30:13 -0500

    Functional direct storage and plots

--- direct_storage.py
+++ direct_storage.py
@@ -12,6 +12,8 @@ import matplotlib.dates as mdates
 import datetime
 from sklearn.metrics import r2_score
 import datetime
+import warnings
+warnings.filterwarnings('ignore')
 
 
 import ee
@@ -20,32 +22,48 @@ ee.Initialize()
 from getPET_har import getRA
 from getPET_har import getPET
 from interp_et import interp_et
-#from dralle_storage import g
-#from dralle_storage import KirchnerBinning
+from dralle_storage import g
+from dralle_storage import KirchnerBinning
 from dralle_storage import recessionAnalysis
+from dralle_storage import storage
+from plots_direct_storage import plot_all_timeseries
+from plots_direct_storage import bar_indirect
+from sklearn.metrics import r2_score
 
 
 def main():
-  
-  basin_name = 'Clear Creek'
+  # Set up imports/exports and names
+  basin_name = 'ClearCreek'
   gage = gage = 11372000 #Clear Creek
   df = pd.read_csv('exports/ClearCreek_ext.csv')
   et_name = 'modis_PET'
+  print('(P)ET dataset being used is ' + et_name)
 
-
+  # Get dataframe in order
   df = getPET(gage, df) # add PET columns using Hargreaves calculated from prism temperature
   df = interp_et(df) # interpolate
   recession = df[['prism_ppt','q_mm', et_name]].rename(columns = {"prism_ppt":"ppt", "q_mm":"q", str(et_name):"et"}).dropna()
   print('Dataframe ready!')
-  print('Recession analysis in progress...')
-  #years, recession = recessionAnalysis(recession)
-  #print('Recession analysis complete.')
-
-
-
-  (print('Number of data using (P)ET dataset ' + et_name + ':{}'.format(len(qs))))
   
+  # Recession analysis
+  years, recession, p, dt = recessionAnalysis(recession, basin_name)
+  print('Recession analysis complete.')
 
+  # Calculate indirect and direct storage using results from recession analysis, update 'recession' df
+  recession, annualmax_indirect, annualmax_direct, maxyears = storage(years, recession, p, dt)
+  
+  #### PLOTS ####
+  
+  # Single water year timeseries
+  start_date = '10-2015'
+  end_date = '4-2016'
+  plot_all_timeseries(recession, dt, basin_name, start_date, end_date)
+  
+  # Bar plots of indirect and direct storage
+  bar_indirect(annualmax_indirect, maxyears, basin_name)
+  
+  #
+  
 if __name__ == "__main__":
   main()
 
