4ba074782d35397a5f1f9d7ac06e9cf24b049402
Erica McCormick <erica.elmstead@gmail.com>
2021-07-26 10:44:59 -0500

    Finished area mask calculation

--- run_basin_vis.sh
+++ run_basin_vis.sh
@@ -1,3 +1,3 @@
 #python3 code/api_extractions/basin_vis.py data/gages_hahm2019.csv gages_hahm_testing2 -mask_check True
 
-python3 code/api_extractions/basin_vis.py data/gages_hahm2019.csv Hahm2019_basins -export_collection True -collection_name HahmBasins
+python3 code/api_extractions/basin_vis.py data/gages_hahm2019.csv Hahm_Unmasked_Areas -calc_unmasked_areas True -exported_collection_name HahmTest
